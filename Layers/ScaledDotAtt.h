6694e14c3f89e97d5115815e96047b01219ca41d
puccinic <cevergarapu@gmail.com>
2023-12-04 13:32:32 +0100

    added constexpr function to scaledotatt to compute scale factor

--- Layers/ScaledDotAtt.h
+++ Layers/ScaledDotAtt.h
@@ -5,6 +5,28 @@
 #include "SoftMax.h"
 #include "Scale.h"
 
+
+/*sqrt_helprt Code taken from: https://stackoverflow.com/questions/8622256/in-c11-is-sqrt-defined-as-constexpr */
+template <typename T>
+constexpr T sqrt_helper(T x, T lo, T hi)
+{
+  if (lo == hi)
+    return lo;
+
+  const T mid = (lo + hi + 1) / 2;
+
+  if (x / mid < mid)
+    return sqrt_helper<T>(x, lo, mid - 1);
+  else
+    return sqrt_helper(x, mid, hi);
+}
+
+template <typename T>
+constexpr T ct_sqrt(T x)
+{
+  return sqrt_helper<T>(x, 0, x / 2 + 1);
+}
+
 template<typename T, int sequence_length, int token_length>
 void scaledotatt(
 	T query[sequence_length][token_length],
@@ -13,7 +35,7 @@ void scaledotatt(
 	T input_mask[sequence_length][sequence_length],
 	T result[sequence_length][token_length]
 ) {
-	const T scale_factor = std::sqrt(token_length);
+	constexpr T scale_factor = ct_sqrt<T>(token_length);
 
 	T queryxkey[sequence_length][sequence_length];
 	matmul_transpose<T, sequence_length, token_length, sequence_length>(query, key, queryxkey);
