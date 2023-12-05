c0c1ac18b76931e6b8f2a99b356d8b163ab5bf6e
puccinic <cevergarapu@gmail.com>
2023-12-05 17:33:56 +0100

    to avoid errors with vitis hls compiler

--- Layers/ScaledDotAtt.h
+++ Layers/ScaledDotAtt.h
@@ -22,10 +22,8 @@ T constexpr sqrtNewtonRaphson(T x, T curr, T prev) {
 *   - Otherwise, returns NaN
 */
 template<typename T>
-double constexpr sqrt(T x) {
-    return x >= 0 && x < std::numeric_limits<T>::infinity()
-        ? sqrtNewtonRaphson<T>(x, x, 0)
-        : std::numeric_limits<T>::quiet_NaN();
+double constexpr constexpr_sqrt(T x) {
+    return sqrtNewtonRaphson<T>(x, x, 0);
 }
 
 template<typename T, int sequence_length, int token_length>
@@ -36,7 +34,7 @@ void scaledotatt(
 	T input_mask[sequence_length][sequence_length],
 	T result[sequence_length][token_length]
 ) {
-	constexpr T scale_factor = sqrt<T>(token_length);
+	constexpr int scale_factor = constexpr_sqrt<int>(token_length);
 	
 	T softmax_att[sequence_length][sequence_length];
 	matmul_scale_masked_softmax<T,sequence_length,token_length,sequence_length>(query, key, scale_factor, input_mask, softmax_att);
