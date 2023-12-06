216480a863f8a8df41438c8ac8448d8fb0d28d54
Carlos Esteban Vergara Puccini (LL) <carlos@olorin.polito.it>
2023-12-06 17:29:54 +0000

    added other test definitions

--- Definitions.h
+++ Definitions.h
@@ -10,9 +10,12 @@
 #define NUM_LINEAR_LAYERS 3
 #define NUM_LAYER_NORM 2
 #define SCALE_FACTOR 3.162277660168379
-typedef int idata_t;
-typedef int odata_t;
+typedef double idata_t;
+typedef double odata_t;
 
+#define ENCODER
+
+#ifdef ENCODER
 void accel(
 	idata_t head_weights[NUM_HEADS][NUM_LINEAR_LAYERS][TOKEN_LEN][HEAD_LEN],
 	idata_t head_biases[NUM_HEADS][NUM_LINEAR_LAYERS][HEAD_LEN],
@@ -28,4 +31,84 @@ void accel(
 	idata_t input_mask[SEQ_LEN][SEQ_LEN],
 	odata_t result[SEQ_LEN][TOKEN_LEN]
 );
+#endif
 
+
+
+#ifdef MULTIHEAD
+void accel(
+	idata_t head_weights[NUM_HEADS][NUM_LINEAR_LAYERS][TOKEN_LEN][HEAD_LEN],
+	idata_t head_biases[NUM_HEADS][NUM_LINEAR_LAYERS][HEAD_LEN],
+	idata_t linear_weights[TOKEN_LEN][TOKEN_LEN],
+	idata_t linear_bias[TOKEN_LEN],
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	idata_t input_mask[SEQ_LEN][SEQ_LEN],
+	odata_t result[SEQ_LEN][SEQ_LEN]
+);
+#endif
+
+#ifdef ATTHEAD
+void accel(
+	idata_t weights[NUM_LINEAR_LAYERS][TOKEN_LEN][HEAD_LEN],
+	idata_t biases[NUM_LINEAR_LAYERS][HEAD_LEN],
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	idata_t input_mask[SEQ_LEN][SEQ_LEN],
+	odata_t result[SEQ_LEN][TOKEN_LEN],
+);
+#endif
+
+#ifdef FDFRWRD
+void accel(
+	idata_t weights1[SEQ_LEN][HIDDEN],
+	idata_t biases1[HIDDEN],
+	idata_t weights2[HIDDEN][TOKEN_LEN],
+	idata_t biases2[TOKEN_LEN],
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	odata_t result[SEQ_LEN][TOKEN_LEN]
+);
+#endif
+
+#ifdef LAYERNORM
+void accel(
+	idata_t gamma[TOKEN_LEN],
+	idata_t beta[TOKEN_LEN],
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	odata_t result[SEQ_LEN][SEQ_LEN]
+);
+#endif
+
+#ifdef DOTPRODATT
+void accel(
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	idata_t input_mask[SEQ_LEN][SEQ_LEN],
+	odata_t result[SEQ_LEN][TOKEN_LEN]
+);
+#endif
+
+#ifdef LINEAR
+void accel(
+	idata_t weights[HIDDEN][TOKEN_LEN],
+	idata_t biases[TOKEN_LEN],
+	idata_t input[SEQ_LEN][HIDDEN],
+	odata_t result[SEQ_LEN][TOKEN_LEN]
+);
+#endif
+
+#ifdef MATMUL
+void accel(
+	idata_t matA[SEQ_LEN][HIDDEN],
+	idata_t matB[HIDDEN][TOKEN_LEN],
+	odata_t matRes[SEQ_LEN][TOKEN_LEN]
+);
+#endif
+
+#ifdef SOFTMAX
+void accel(idata_t input[SEQ_LEN], idata_t result[SEQ_LEN]);
+#endif
+
+#ifdef ACTIVATION
+void accel(
+	idata_t input[SEQ_LEN][TOKEN_LEN],
+	odata_t result[SEQ_LEN][TOKEN_LEN]
+);
+#endif
