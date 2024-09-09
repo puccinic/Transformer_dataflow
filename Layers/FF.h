e5085377bdb3a84ccc860b6e00b2897ce97b7b91
puccinic <55330220+puccinic@users.noreply.github.com>
2024-09-09 12:46:54 +0200

    Update FF.h
    modified depth of stream

--- Layers/FF.h
+++ Layers/FF.h
@@ -13,8 +13,8 @@ void ff(
 	hls::stream<rT>& result
 )
 {
-	hls::stream<tmpT, rows> ff_tmp1{};
-	hls::stream<tmpT, rows> ff_tmp2{};
+	hls::stream<tmpT, rows*hidden> ff_tmp1{};
+	hls::stream<tmpT, rows*cols> ff_tmp2{};
 
 	#pragma HLS DATAFLOW
 	matmul_transpose<iT, w1T, tmpT, rows, cols, hidden>(input, weights1, ff_tmp1);
